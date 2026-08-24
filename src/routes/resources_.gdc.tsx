866b5d959bdd910292fafd01ab3d2abfcabe93ff
bashir Mosayyebi <119165074+Bashir1370@users.noreply.github.com>
2026-08-24 21:47:37 +0330

    Prepare GDC page route for Supabase managed content

--- src/routes/resources_.gdc.tsx
+++ src/routes/resources_.gdc.tsx
@@ -1,9 +1,11 @@
 import { createFileRoute } from "@tanstack/react-router";
 
 import { GdcHomeTour } from "@/features/data-resources/gdc-home";
+import { loadResourceTour } from "@/features/data-resources/resource-tour-loader";
 
 export const Route = createFileRoute("/resources/gdc")({
   component: GdcResourcePage,
+  loader: async () => loadResourceTour("gdc"),
 });
 
 function GdcResourcePage() {
